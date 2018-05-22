7494162279761519411d6096226cee188bb0952e
Chris Stull <39490397+DownStatic@users.noreply.github.com>
2018-05-22 13:51:44 -0400

    Update TicTacToe_Functions.py

--- TicTacToe_Functions.py
+++ TicTacToe_Functions.py
@@ -1,5 +1,8 @@
+#Function definitions
+
 from IPython.display import clear_output
 
+#This function displays the current board
 def display_board(board):
     print("           |          |          ")
     print("     "+board[1]+"     |     "+board[2]+"    |     "+board[3]+"     ")
@@ -13,16 +16,19 @@ def display_board(board):
     print("     "+board[7]+"     |     "+board[8]+"    |     "+board[9]+"     ")
     print("           |          |          ")
 
+#This function determines which player gets which mark
 def player_input():
     player1 = "a"
     while not player1 in ["X","O"]:
         player1 = input("Please select X or O")
     return player1
-    
+
+#This function places the appropriate mark at a spot on the board
 def place_marker(board, marker, position):
     board[position] = marker
     pass
 
+#This function checks to see if anyone has won the game
 def win_check(board, mark):
     if board[1]==mark and board[2]==mark and board[3]==mark:
         return True
@@ -43,37 +49,48 @@ def win_check(board, mark):
     
 import random
 
-def choose_first():
+#This function randomly picks between two values 
+#and says who/which mark goes first.
+def choose_first(player1,player2):
     first = 0
     first = random.randint(1,2)
     if first==1:
-        print("X goes first")
-    elif first ==2:
-        print("O goes first")
+        print(player1+" goes first")
+    elif first==2:
+        print(player2+" goes first")
     return first   
 
+#This function checks if a spot is open
 def space_check(board, position):
     return board[position]==" "
     pass
 
+#This function checks if the board is full
 def full_board_check(board):
     return not " " in board
     pass
 
+#This function allows the player to select a spot on the board
+#and makes sure it is an integer between 1 and 9
 def player_choice(board):
     selection = 0
     while not space_check(board,selection):
-        selection = int(input("Pick a spot 1 through 9"))
-        if not type(selection) == int:
-            print("Please choose a number 1 through 9")
-            selection = input("Pick a spot 1 through 9")
-        
-        if space_check(board,selection):
-            return int(selection)
-            break
+        try:
+            selection = int(input("Pick a spot 1 through 9"))
+        except ValueError:
+            print("Your entry was invalid.")
         else:
-            print("That spot's already taken")
-
+            if selection in range(1,10):
+                if space_check(board,selection):
+                    return selection
+                    break
+                else:
+                    print("That spot's already taken")
+            else:
+                selection = 0
+                print("Your entry was invalid.")
+                
+#This function checks for whether the game will be replayed.                
 def replay():
     again = ""
     while again not in ["Yes","y","yes","Y","No","no","N","n"]:
